cb8f83a521b344ca32129b3ee43416e499c31d5a
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-01-11 18:33:18 +0530

    Python Program to print star of different colors using turtle library of python.

--- stars.py
+++ stars.py
@@ -5,3 +5,25 @@
 #   on 28/12/21  at   11:36 PM
 #
 # # # # # # # # # # # # # # # # #
+
+
+import colorsys as csys
+import turtle as t
+
+T = t.Turtle()
+S = t.Screen()
+
+
+S.bgcolor('white')
+T.speed(8)
+
+n = 20
+h = 10
+
+T.left(107)
+for i in range(100, 0, -1):
+    c = csys.hsv_to_rgb(h, 1, 0.9)
+    h += 1/n
+    T.color(c)
+    T.left(144)
+    T.forward(5 * i)
